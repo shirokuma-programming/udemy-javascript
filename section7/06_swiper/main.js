93556590465324526b427c383ee8890fbb0b8759
shirokuma-programming <shirokuma2294@gmail.com>
2022-11-13 12:04:12 +0900

    ヒーロースライダーの作成

--- section7/06_swiper/main.js
+++ section7/06_swiper/main.js
@@ -1,21 +1,5 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  // direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+document.addEventListener("DOMContentLoaded", function () {
+  const hero = new HeroSlider(".swiper");
+  hero.start({delay: 3000});
 });
+
