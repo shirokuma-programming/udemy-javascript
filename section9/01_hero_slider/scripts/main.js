076105432e3dc801c43f62220bdafd602f12a452
shirokuma-programming <shirokuma2294@gmail.com>
2022-11-21 23:05:13 +0900

    関数名の修正

--- section9/01_hero_slider/scripts/main.js
+++ section9/01_hero_slider/scripts/main.js
@@ -2,13 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
   const hero = new HeroSlider('.swiper');
   hero.start();
 
-  const cb = function(el, isIntersecting) {
+  const _textAnimation = function(el, isIntersecting) {
     if (isIntersecting) {
       const ta = new TweenTextAnimation(el);
       ta.animate();
     }
   }
-  const so = new ScrollObserver('.tween-animate-title', cb);
+  const so = new ScrollObserver('.tween-animate-title', _textAnimation);
 
   const _inviewAnimation = function(el, inview) {
     if (inview) {
