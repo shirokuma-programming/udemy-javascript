aa5f35a9b90265be77215a1f1de13e68f9d409cf
shirokuma-programming <shirokuma2294@gmail.com>
2022-11-21 22:42:37 +0900

    タイトル、サブタイトルの追加

--- section9/01_hero_slider/scripts/main.js
+++ section9/01_hero_slider/scripts/main.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
   const hero = new HeroSlider('.swiper');
   hero.start();
+
+  const cb = function(el, isIntersecting) {
+    if (isIntersecting) {
+      const ta = new TweenTextAnimation(el);
+      ta.animate();
+    }
+  }
+  const so = new ScrollObserver('.tween-animate-title', cb);
 });
